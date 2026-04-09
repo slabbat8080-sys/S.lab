893d1803de1423d689960bf8be24f9b99d22862e
Nirmaan <s.lab.bat8080@gmail.com>
2026-04-09 11:01:36 +0530

    fix: resolve global bat path bug

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 const { exec } = require("child_process");
+const path = require("path");
 
-exec("S.lab.bat", (err, stdout) => {
+const batPath = path.join(__dirname, "S.lab.bat");
+
+exec(`"${batPath}"`, (err, stdout) => {
+  if (err) {
+    console.error("Error executing script:", err);
+    return;
+  }
   console.log(stdout);
 });
